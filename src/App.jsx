d9ddf5887cae443407f610c9197291c3db465e2f
osumsky <osumsky@gmail.com>
2022-11-07 13:07:37 +0200

    lesson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css';
 import user from './user.json';
 
-function Greeting(props) {
-  const { name } = props;
-  return <h1 className="greeting">Hello. {name}. Welcome!</h1>;
-}
-
 export default function App() {
-  return <Greeting name="Oleg" />;
+  return (
+    <main
+      onClick={(e) => {
+        console.log('main');
+        console.log(e.target);
+        console.log(e.currentTarget);
+      }}
+      onClickCapture={(e) => {
+        // e.stopPropagation();
+        console.log('main (on capturing)');
+      }}
+    >
+      <form
+        onClick={(e) => {
+          console.log('form');
+        }}
+      >
+        <label
+          onClick={(e) => {
+            e.preventDefault();
+            console.log('label');
+          }}
+        >
+          field
+          <input type="text" />
+        </label>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            console.log('onSubmit');
+          }}
+        >
+          Submit
+        </button>
+      </form>
+    </main>
+  );
 }
