72de47947c1f097d060f0e1336d7ed75b19cefbc
osumsky <osumsky@gmail.com>
2022-12-20 22:24:00 +0200

    recoding with redux hooks

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,12 +1,17 @@
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import * as UserActionCreators from '../actions/userCreactors';
 
 function UsersList(props) {
-  const { users, isFetching, error, getUsersReq } = props;
+  // const { users, isFetching, error, getUsersReq } = props;
+  const { users, isFetching, error } = useSelector(({ users }) => users);
+  const dispatch = useDispatch();
+  const loadUsers = ({ limit, offset }={}) =>
+    dispatch(UserActionCreators.getUsersRequest({ offset: users.length }));
 
   useEffect(() => {
-    getUsersReq({});
+    loadUsers();
   }, []);
 
   return (
@@ -18,17 +23,17 @@ function UsersList(props) {
       {users.map((user) => (
         <li key={user.id}>{user.email}</li>
       ))}
-      <button onClick={() => getUsersReq({ offset: users.length })}>
-        Load more
-      </button>
+      <button onClick={() => loadUsers()}>Load more</button>
     </section>
   );
 }
 
-const mapStateToProps = ({ users }) => users;
-const mapDispatchToProps = (dispatch) => ({
-  getUsersReq: ({ limit, offset }) =>
-    dispatch(UserActionCreators.getUsersRequest({ limit, offset })),
-});
+// Used with "connect" function from 'react-redux'
+// const mapStateToProps = ({ users }) => users;
+// const mapDispatchToProps = (dispatch) => ({
+//   getUsersReq: ({ limit, offset }) =>
+//     dispatch(UserActionCreators.getUsersRequest({ limit, offset })),
+// });
+// export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
+export default UsersList;
